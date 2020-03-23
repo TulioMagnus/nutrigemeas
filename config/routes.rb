0fef042f2c7a9485c7599e5c3d7ba0d47d92606c
Tulio Magnus <tuliokpmagnus@gmail.com>
2020-03-23 20:58:42 -0300

    fixed pacients to sidebar and fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :dashboard do
-    get 'pacients/index'
-  end
   root 'dashboard/welcome#index'
   namespace :dashboard do
     get 'welcome/index'
+    resources :pacients
   end
   devise_for :users, controllers: {
     sessions: 'users/sessions'
