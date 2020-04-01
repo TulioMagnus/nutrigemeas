c40b8ddb164d0f70c2e7b40cb873562b1dc3d745
Tulio Magnus <tuliokpmagnus@gmail.com>
2020-04-01 15:06:32 -0300

    removed consultation since appointment is most apropriate

--- app/models/consultation.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Consultation < ApplicationRecord
-  belongs_to :pacient
-  has_many :appointments
-end
--- spec/models/consultation_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe Consultation, type: :model do
-  it { should belong_to(:pacient) }
-end
