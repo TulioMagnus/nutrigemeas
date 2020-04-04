c25258c9b9efd6b0c0747e7332883f3947aee7c7
Tulio Magnus <tuliokpmagnus@gmail.com>
2020-04-04 10:57:58 -0300

    added controller actions to welcome#index

--- app/controllers/dashboard/welcome_controller.rb
+++ app/controllers/dashboard/welcome_controller.rb
@@ -4,7 +4,8 @@ class Dashboard::WelcomeController < DashboardController
   def index
     @count_pacients = Pacient.count
     @count_appointments = Appointment.count
-    @total_earned = Appointment.sum(:price_cents)
+    @total_earned = Appointment.where(payment_status: true).sum(:price_cents)
+    @total_to_earn = Appointment.where(payment_status: false).sum(:price_cents)
     @appointments_per_month = Appointment.all.group_by_month(:appointment_date, last: 12, format: '%b %y').count
   end
 end
