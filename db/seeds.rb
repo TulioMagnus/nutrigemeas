8e3f95ad4235ef07f31883a6d7f722452708c83b
Tulio Magnus <tuliokpmagnus@gmail.com>
2020-04-26 17:36:14 -0300

    adding seeds for patient

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,8 @@ Tag.create(name: 'Obeso(a)')
 Tag.create(name: 'Diabético(a)')
 Tag.create(name: 'Emagrecer')
 Tag.create(name: 'Hipertrofia')
+Patient.create(first_name: 'otavio', last_name: 'schwanck', birth_date: Time.zone.now,
+               profession: 'Senior Ruby on Rails Dev', gender: 'Male', street: 'Rua X',
+               number: 15, state: 'RS', country: 'Brazil', city: 'Torres', cpf: '00000000000',
+               email: 'otavioschwanck@gmail.com', obs: 'moreno alto, bonito e sensual',
+               phone: '51996312587', status: 'active', user_id: 1)
